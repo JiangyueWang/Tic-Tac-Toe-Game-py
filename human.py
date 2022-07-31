7a9a05c01890b2db1ff1493732de0cb1f7a1ae8a
Jiang <jiangyue0380@gmail.com>
2022-07-31 19:24:48 +1000

    updated choose_symbol and make_move methods in human.py

--- human.py
+++ human.py
@@ -7,7 +7,7 @@ class Human():
 
     def choose_symbol(self):
         while True:
-            self.user_input = input("Do you want to be 'X' or 'O'?").upper()
+            self.user_input = input("Do you want to be 'X' or 'O'?\n").upper()
             if self.user_input == 'X':
                 self.symbol = 'X'
                 break
@@ -18,4 +18,21 @@ class Human():
                 print("Check your entry, select again")
 
     def make_move(self):
-        pass
+        while True:
+            self.position = input(
+                f"Please enter 1 to 9 to represent the postion you want to place your symbol:\n")
+            try:
+                self.position = int(self.position)
+                if self.position not in range(0, 10):
+                    print(f"Check your input, try again\n")
+                    continue
+                else:
+                    break
+            except ValueError:
+                print(f"Must be a valid integer, try again\n")
+                continue
+
+
+# human_one = Human("Human 1")
+# human_one.choose_symbol()
+# human_one.make_move()
